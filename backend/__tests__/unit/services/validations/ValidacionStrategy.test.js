ce5605e8365d15d6a9a8f407ac1f9f397415494d
Eduardo Necul <enecul2025@alu.uct.cl>
2026-07-01 19:52:53 -0400

    test: añade test unitario a ValidacionStrategy.js

---
+++ backend/__tests__/unit/services/validations/ValidacionStrategy.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from '@jest/globals';
+import { ValidacionStrategy } from '../../../../services/validations/ValidacionStrategy.js';
+
+describe('ValidacionStrategy (Clase Base)', () => {
+    it('debería lanzar un error indicando que debe ser implementado por el hijo', async () => {
+        const estrategia = new ValidacionStrategy();
+        await expect(estrategia.validar({})).rejects.toThrow("El método 'validar' debe ser implementado por la estrategia concreta.");
+    });
+});
